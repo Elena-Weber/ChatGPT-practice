6aa378b17b020541031ea75195b4c35a65fdc04a
Elena-Weber <crochetmiracles@gmail.com>
2023-09-30 18:03:05 -0400

    update placeholder

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -148,7 +148,8 @@ export default function Dashboard() {
               value={joke}
               disabled={joke === ""}
               id="output"
-              placeholder="AI Generated Joke"
+              // placeholder="AI Generated Joke"
+              placeholder="Sorry this app doesn't work anymore. It's run out of requests quota."
               className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
             />
             <button
